3f5561ca640e9ec44559e07cf034e53864f8f318
Kristina Zhukova <litvin.kris@yandex.ru>
2024-06-30 18:52:32 +0300

    New commit

--- tests/test_password_recovery_page.py
+++ tests/test_password_recovery_page.py
@@ -1,5 +1,4 @@
 import allure
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from data.urls import URLS, MainUrl
